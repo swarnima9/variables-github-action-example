06775ae49001f90e0887a59f4831bd7205c789f8
swarnima9 <swarnima.chandra96@gmail.com>
2024-07-05 11:43:36 +0100

    Update sync_figma_to_tokens.ts

--- src/sync_figma_to_tokens.ts
+++ src/sync_figma_to_tokens.ts
@@ -17,12 +17,13 @@ import { tokenFilesFromLocalVariables } from './token_export.js'
  */
 
 async function main() {
-  if (!process.env.PERSONAL_ACCESS_TOKEN || !process.env.FILE_KEY) {
-    throw new Error('PERSONAL_ACCESS_TOKEN and FILE_KEY environemnt variables are required')
-  }
-  const fileKey = process.env.FILE_KEY
-
-  const api = new FigmaApi(process.env.PERSONAL_ACCESS_TOKEN)
+  //if (!process.env.PERSONAL_ACCESS_TOKEN || !process.env.FILE_KEY) {
+  // throw new Error('PERSONAL_ACCESS_TOKEN and FILE_KEY environemnt variables are required')
+  //}
+  //const fileKey = process.env.FILE_KEY
+  const fileKey = '7jAP0w1JIJH7yHifz2lhC9'
+  //const api = new FigmaApi(process.env.PERSONAL_ACCESS_TOKEN)
+  const api = new FigmaApi('figd_s8ZpWk1a0nty8cF8Y0x4rhFL6f2JIYxaWtxeEeRV')
   const localVariables = await api.getLocalVariables(fileKey)
 
   const tokensFiles = tokenFilesFromLocalVariables(localVariables)
